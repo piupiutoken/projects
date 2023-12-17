b1ed050105cfe98085fc73608522981dc2bba6e0
rajurajkishoreSP <124571527+rajurajkishoreSP@users.noreply.github.com>
2023-12-17 17:52:46 +0530

    Delete 2023/Arthur Exchange directory
    Changing to flashpad

--- 2023/Arthur Exchange/contracts/pool/contracts/interfaces/IXArtTokenUsage.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
-
-interface IXArtTokenUsage {
-    function allocate(address userAddress, uint256 amount, bytes calldata data) external;
-    function deallocate(address userAddress, uint256 amount, bytes calldata data) external;
-}
